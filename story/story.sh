d7fea66fb4bc5065a576fd4fb08ad9aa393b48d0
Elessar <108255403+enyaselessar@users.noreply.github.com>
2024-10-12 22:18:11 +0600

    Update story.sh

--- story/story.sh
+++ story/story.sh
@@ -24,20 +24,19 @@ echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
 source ~/.bash_profile
 go version
 
-# Step 4: Download and Install Story-Geth Binary
-printGreen "Downloading and installing Story-Geth binary..." && sleep 1
-wget -q https://story-geth-binaries.s3.us-west-1.amazonaws.com/geth-public/geth-linux-amd64-0.9.3-b224fdf.tar.gz -O /tmp/geth-linux-amd64-0.9.3-b224fdf.tar.gz
-tar -xzf /tmp/geth-linux-amd64-0.9.3-b224fdf.tar.gz -C /tmp
-[ ! -d "$HOME/go/bin" ] && mkdir -p $HOME/go/bin
-sudo cp /tmp/geth-linux-amd64-0.9.3-b224fdf/geth $HOME/go/bin/story-geth
+# Step 4: Download and Install Story Story-Geth Binary
+printGreen "Downloading and installing Story Story-Geth binaries..." && sleep 1
+cd $HOME
+rm -rf bin
+mkdir bin
+cd bin
+wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/geth-public/geth-linux-amd64-0.9.3-b224fdf.tar.gz
+tar -xvzf geth-linux-amd64-0.9.3-b224fdf.tar.gz
+mv ~/bin/geth-linux-amd64-0.9.3-b224fdf/geth ~/go/bin/
+[ ! -d "$HOME/.story/story" ] && mkdir -p "$HOME/.story/story"
+[ ! -d "$HOME/.story/geth" ] && mkdir -p "$HOME/.story/geth"
 
-# Step 5: Download and Install Story Binary
-printGreen "Downloading and installing Story binary..." && sleep 1
-wget -q https://story-geth-binaries.s3.us-west-1.amazonaws.com/story-public/story-linux-amd64-0.10.1-57567e5.tar.gz -O /tmp/story-linux-amd64-0.10.1-57567e5.tar.gz
-tar -xzf /tmp/story-linux-amd64-0.10.1-57567e5.tar.gz -C /tmp
-sudo cp /tmp/story-linux-amd64-0.10.1-57567e5/story $HOME/go/bin/story
-
-# Step 6: Install Story
+# Step 5: Install Story
 printGreen "Installing Story..." && sleep 1
 cd $HOME
 rm -rf story
@@ -47,11 +46,11 @@ git checkout v0.11.0
 go build -o story ./client 
 mv $HOME/story/story $HOME/go/bin/
 
-# Step 7: Initialize the Iliad Network Node
+# Step 6: Initialize the Iliad Network Node
 printGreen "Initializing Iliad network node..." && sleep 1
 $HOME/go/bin/story init --network iliad
 
-# Step 8: Create and Configure systemd Service for Story-Geth
+# Step 7: Create and Configure systemd Service for Story-Geth
 printGreen "Creating systemd service for Story-Geth..." && sleep 1
 sudo tee /etc/systemd/system/story-geth.service > /dev/null <<EOF
 [Unit]
@@ -69,7 +68,7 @@ LimitNOFILE=4096
 WantedBy=multi-user.target
 EOF
 
-# Step 9: Create and Configure systemd Service for Story
+# Step 8: Create and Configure systemd Service for Story
 printGreen "Creating systemd service for Story..." && sleep 1
 sudo tee /etc/systemd/system/story.service > /dev/null <<EOF
 [Unit]
@@ -87,11 +86,11 @@ LimitNOFILE=4096
 WantedBy=multi-user.target
 EOF
 
-# Step 10: Ask for Moniker and Update config.toml
+# Step 9: Ask for Moniker and Update config.toml
 printGreen "Please enter the moniker for your node (e.g., your node's name):" && sleep 1
 read -r moniker
 
-# Step 11: Update Ports in config.toml Based on User Input
+# Step 10: Update Ports in config.toml Based on User Input
 printGreen "Please enter the starting port number (between 11 and 64). Default is 26:" && sleep 1
 read -r start_port
 
@@ -133,17 +132,19 @@ printLine
 sleep 1
 
 
-# Step 12: Update Persistent Peers in config.toml
-printGreen "Fetching peers and updating persistent_peers in config.toml..." && sleep 1
-URL="https://story-testnet-rpc.itrocket.net/net_info"
-response=$(curl -s $URL)
-PEERS=$(echo $response | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):" + (.node_info.listen_addr | capture("(?<ip>.+):(?<port>[0-9]+)$").port)' | paste -sd "," -)
-echo "PEERS=\"$PEERS\""
+# Step 11: Update Persistent Peers and Seed in config.toml
+printGreen "Fetching peers-seed and updating persistent_peers in config.toml..." && sleep 1
+SEEDS="51ff395354c13fab493a03268249a74860b5f9cc@story-testnet-seed.itrocket.net:26656"
+PEERS="2f372238bf86835e8ad68c0db12351833c40e8ad@story-testnet-peer.itrocket.net:26656,fa58ccd87f82aec19746b8908c30fd2a712122c3@212.192.222.42:26656,a932908520c8bf2514b0eadd1952e97b04e02813@194.163.167.132:26656,4e159edf6e7affa518bba93c0e25a2d7bd36e187@185.197.251.19:26656,3833efddd6665ffaf20950abad7c8bb4918c0161@65.109.111.234:26656,67630373dd14bd9cd2a5beb42d3ad3255b370aa3@194.163.133.213:26656,fcd591a5462974bf684ae596a87796ab56d7a64e@212.192.222.59:26656,3e0cde7382067bc449ec1ad979e136d5de774732@202.61.201.53:26656,2415dfb9dbf3b3ee77824697127aecab87d18598@176.9.54.69:26656,90161a7f82ce5dbfbed1a2a9d40d4103730cff0f@5.9.87.231:26656,1f4c8031c89661f214678ea5b6157a7a000d994f@109.199.100.6:26656"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" $HOME/.story/story/config/config.toml
 
-# Update the persistent_peers in the config.toml file
-sed -i 's|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $CONFIG_PATH
+echo "Persistent peers and seed updated in $CONFIG_PATH."
 
-echo "Persistent peers updated in $CONFIG_PATH."
+# Step 12: Download genesis and addrbook
+printGreen "Downloading genesis and addrbook..." && sleep 1
+wget -O $HOME/.story/story/config/genesis.json https://server-3.itrocket.net/testnet/story/genesis.json
+wget -O $HOME/.story/story/config/addrbook.json  https://server-3.itrocket.net/testnet/story/addrbook.json
 
 
 # Step 13: Reload systemd, Enable, and Start Services
